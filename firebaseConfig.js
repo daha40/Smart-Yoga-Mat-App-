b48ef8b71c9b5c347ea2e683036a3cb653e8ff44
Abderahmane Kateb <64675525+daha40@users.noreply.github.com>
2024-11-19 23:05:48 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAcK5q90hHCTbE0bt05mweF06RvEnu9wLk",
-  authDomain: "smart-yoga-mat-34948.firebaseapp.com",
-  projectId: "smart-yoga-mat-34948",
-  storageBucket: "smart-yoga-mat-34948.firebasestorage.app",
-  messagingSenderId: "507901373433",
-  appId: "1:507901373433:web:f84a7179aa981e3fa4ab28",
-  measurementId: "G-CJD6CBDMZG"
+  apiKey: "///",
+  authDomain: "///",
+  projectId: "///",
+  storageBucket: "///",
+  messagingSenderId: "///",
+  appId: "///",
+  measurementId: "///"
 };
 
 const app = initializeApp(firebaseConfig);
